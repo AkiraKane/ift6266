943a4a852626ba9595528fba810506ae962da62d
Thomas George <ift6ed03@ngpu-a4-04.rqchp.qc.ca>
2016-03-21 13:58:23 -0400

    added random horizontal flip

---
+++ catsdogs/transformers.py
@@ -0,0 +1,22 @@
+from fuel.transformers import SourcewiseTransformer
+import numpy
+import random
+
+
+class RandomHorizontalFlip(SourcewiseTransformer):
+    """
+
+    """
+    def __init__(self, data_stream, **kwargs):
+        kwargs.setdefault('produces_examples', data_stream.produces_examples)
+        super(RandomHorizontalFlip, self).__init__(data_stream, **kwargs)
+
+    def transform_source_batch(self, batch, source_name):
+        return [self._example_transform(im, source_name) for im in batch]
+
+    def transform_source_example(self, example, source_name):
+        return self._example_transform(example, source_name)
+
+    def _example_transform(self, example, source_name):
+        flip = random.randint(0, 1)*2-1
+        return example[:,:,::flip]
