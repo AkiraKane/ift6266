d264314c77a06a60eb06d606dd244ff12550fc44
Thomas George <tfjgeorge@gmail.com>
2016-04-02 12:31:09 -0400

    full mode for convs

--- catsdogs/models/vggnet_bricks.py
+++ catsdogs/models/vggnet_bricks.py
@@ -14,9 +14,9 @@ class VGGNet(FeedforwardSequence, Initializable):
         
         #############################################
         # a first block with 2 convolutions of 32 (3, 3) filters
-        layers.append(Convolutional((3, 3), 32))
+        layers.append(Convolutional((3, 3), 32, border_mode='full'))
         layers.append(Rectifier())
-        layers.append(Convolutional((3, 3), 32))
+        layers.append(Convolutional((3, 3), 32, border_mode='full'))
         layers.append(Rectifier())
 
         # maxpool with size=(2, 2)
@@ -24,11 +24,11 @@ class VGGNet(FeedforwardSequence, Initializable):
 
         #############################################
         # a 2nd block with 3 convolutions of 64 (3, 3) filters
-        layers.append(Convolutional((3, 3), 64))
+        layers.append(Convolutional((3, 3), 64, border_mode='full'))
         layers.append(Rectifier())
-        layers.append(Convolutional((3, 3), 64))
+        layers.append(Convolutional((3, 3), 64, border_mode='full'))
         layers.append(Rectifier())
-        layers.append(Convolutional((3, 3), 64))
+        layers.append(Convolutional((3, 3), 64, border_mode='full'))
         layers.append(Rectifier())
         
         # maxpool with size=(2, 2)
@@ -36,13 +36,13 @@ class VGGNet(FeedforwardSequence, Initializable):
 
         #############################################
         # a 3rd block with 4 convolutions of 128 (3, 3) filters
-        layers.append(Convolutional((3, 3), 128))
+        layers.append(Convolutional((3, 3), 128, border_mode='full'))
         layers.append(Rectifier())
-        layers.append(Convolutional((3, 3), 128))
+        layers.append(Convolutional((3, 3), 128, border_mode='full'))
         layers.append(Rectifier())
-        layers.append(Convolutional((3, 3), 128))
+        layers.append(Convolutional((3, 3), 128, border_mode='full'))
         layers.append(Rectifier())
-        layers.append(Convolutional((3, 3), 128))
+        layers.append(Convolutional((3, 3), 128, border_mode='full'))
         layers.append(Rectifier())
         
         # maxpool with size=(2, 2)
@@ -62,6 +62,8 @@ class VGGNet(FeedforwardSequence, Initializable):
     def _push_allocation_config(self):
         self.conv_sequence._push_allocation_config()
         conv_out_dim = self.conv_sequence.get_dim('output')
+
+        print conv_out_dim
         
         self.top_mlp.dims = [numpy.prod(conv_out_dim)] + self.top_mlp.dims
 
